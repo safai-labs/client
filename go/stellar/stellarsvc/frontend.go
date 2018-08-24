ad1f829761f7137fee92a48b0b4517b0d2248158
Michał Zochniak <michal@zapu.net>
2018-08-24 19:18:54 +0200

    Add TxID to SendPaymentLocal result, because why not?

--- go/stellar/stellarsvc/frontend.go
+++ go/stellar/stellarsvc/frontend.go
@@ -1282,6 +1282,7 @@ func (s *Server) SendPaymentLocal(ctx context.Context, arg stellar1.SendPaymentL
 	return stellar1.SendPaymentResLocal{
 		KbTxID:  sendRes.KbTxID,
 		Pending: sendRes.Pending,
+		TxID:    sendRes.TxID,
 	}, nil
 
 }
